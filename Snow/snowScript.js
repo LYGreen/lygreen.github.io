54cc1f123cd3db26c5f2ec122fdc4aef8e41e83a
LYGreen <88890606+LYGreen@users.noreply.github.com>
2024-06-22 17:36:17 +0800

    Add files via upload

--- Snow/snowScript.js
+++ Snow/snowScript.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
         snowflake.className = "snowflake";
 
         const size = Math.random() * 5 + 5 + 'px'; // 雪花大小
-        const positionX = Math.random() * 96 + 2 + 'vw'; // 雪花水平位置
+        const positionX = Math.random() * 94 + 3 + 'vw'; // 雪花水平位置
         const duration = Math.random() * 5 + 5 + 's'; // 雪花下落时间
         const delay = Math.random() * -20 + 's'; // 雪花延迟时间
 
