0125956c9e946ee6614ce26087914410575be67a
LYGreen <3098036603@qq.com>
2026-02-14 03:29:18 +0800

    improve localtime string

--- src/components/LocalTime.vue
+++ src/components/LocalTime.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
 
 const { date } = defineProps<{
     date: Date
 }>();
 
-const placeholder = ref('Loading...');
-
-onMounted(() => {
-    placeholder.value = date.toLocaleString();
-});
-
 </script>
 
 <template>
-    <span>{{ placeholder }}</span>
+    <span>{{ date.toLocaleString() }}</span>
 </template>
