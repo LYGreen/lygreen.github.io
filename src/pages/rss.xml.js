888d8c774f46ea9b8764de65995dc7d30bd5aa8a
LYGreen <3098036603@qq.com>
2026-02-14 02:56:22 +0800

    rss feed item limit

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,11 +4,12 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
     const blog = await getCollection("blog");
     const sortedPosts = blog.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime());
+    const slicedPosts = sortedPosts.slice(0, 10);
     return rss({
         title: 'LYGreen\'s blog',
         description: 'My personal blog, sharing coding adventures, tech notes, and creative ideas.',
         site: context.site,
-        items: sortedPosts.map((post) => {
+        items: slicedPosts.map((post) => {
             return {
                 title: post.data.title,
                 description: post.data.description,
