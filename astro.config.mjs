afc835ab135ec009c83f0a88f7565c45a02c1f77
LYGreen <3098036603@qq.com>
2026-02-18 01:24:08 +0800

    KaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import vue from '@astrojs/vue';
-
 import sitemap from "@astrojs/sitemap";
 
 const domain = 'https://lygreen.top';
@@ -10,8 +10,12 @@ const domain = 'https://lygreen.top';
 // https://astro.build/config
 export default defineConfig({
   site: `${domain}`,
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   integrations: [
-    vue(), 
+    vue(),
     sitemap({
       filter: (page) => {
         return page.startsWith(`${domain}/posts/`) || page === `${domain}`;
